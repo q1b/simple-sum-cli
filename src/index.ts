705db042966ab3191861fcfa3a7a7be845fd0faa
Sukhpreet Singh <sukhpreetben10@gmail.com>
2024-07-28 00:41:01 +0530

    demo source code for publishing the package

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
 console.log("Hello, World!");
 
-export const sum = (...nums: number[]) => {
+const sum = (...nums: number[]) => {
     return nums.reduce((acc, num) => acc + num, 0);
 };
+
+const main = async () => {
+    console.log(sum(1, 2, 3, 4, 5));
+}
+
+
+main().catch((err) => {
+    if (err instanceof Error) {
+      console.log(err);
+    }
+    process.exit(1);
+});
