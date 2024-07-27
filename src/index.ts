6b33e1cde89b279e5968e222cc1e02bc5ef5ffce
Sukhpreet Singh <sukhpreetben10@gmail.com>
2024-07-28 01:35:07 +0530

    simple sum cli application

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,30 @@
 #!/usr/bin/env node
+import { confirm, intro, outro, text } from '@clack/prompts';
+
 const sum = (...nums: number[]) => {
     return nums.reduce((acc, num) => acc + num, 0);
 };
 
 const main = async () => {
-    console.log(sum(1, 2, 3, 4, 5));
+    intro(`Simple Sum Application`);
+    let nums = [];
+    let shouldContinue = false;
+    do {
+        const num = await text({
+            message: 'Enter a number',
+            placeholder: '43',
+            validate(value) {
+                if (value.length === 0) return `Value is required!`;
+                if (isNaN(Number(value))) return `Value must be a number!`;
+            },
+        });
+        nums.push(parseInt(num.toString()));
+        shouldContinue = await confirm({
+            message: 'Do you want to continue?',
+        }) as boolean;
+    } while (shouldContinue);
+    outro(`total sum is ${sum(...nums)}`);
+    process.exit(1);
 }
 
 main().catch((err) => {
