ea6f78a08d7e9892debba48908c94f60fb2b239a
Sukhpreet Singh <sukhpreetben10@gmail.com>
2024-07-28 00:50:10 +0530

    updated package.json to accommodate the cli tooling system

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,1 @@
-// src/index.ts
-console.log("Hello, World!");
-var sum = (...nums) => {
-  return nums.reduce((acc, num) => acc + num, 0);
-};
-export {
-  sum
-};
+console.log("Hello, World!");var e=(...o)=>o.reduce((c,n)=>c+n,0),s=async()=>{console.log(e(1,2,3,4,5))};s().catch(o=>{o instanceof Error&&console.log(o),process.exit(1)});
